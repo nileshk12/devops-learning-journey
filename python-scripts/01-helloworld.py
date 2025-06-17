ff298d71f573781f3b9a19c5ed888565714d970f
Nilesh Kundu <nilesh.kundun@gmail.com>
2025-06-17 23:12:31 +0530

    commentting and sum and product of numbers

--- python-scripts/01-helloworld.py
+++ python-scripts/01-helloworld.py
@@ -1,3 +1,22 @@
 print("Hello World")
 print('Hello''World')
 print ("Hello")
+
+# Python comments
+# Print a number
+print(20)
+
+# Single line comment
+# declare a variable
+name = 'John'
+# print name
+print(name) #John
+
+number1 = 10
+number2 = 20
+
+sum = number1 + number2
+product = number1 * number2
+
+print("the sum is", sum)
+print("the product is", product)
